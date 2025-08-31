cd67c6fbe9afa91b2c6f76de2ce9f8786a418fc7
Leon Farrel <leonfarrel09@gmail.com>
2025-08-31 22:04:27 +0800

    Update API Data

--- src/test/java/negative_case/TestPatchUserNegative.java
+++ src/test/java/negative_case/TestPatchUserNegative.java
@@ -22,7 +22,7 @@ public class TestPatchUserNegative {
                 .accept("application/json")
                 .body(bodyObj.toString())
                 .when()
-                .patch("https://gorest.co.in/public/v2/users/8098381")
+                .patch("https://gorest.co.in/public/v2/users/8097968")
                 .then().log().all()
                 .assertThat().statusCode(422)
                 .assertThat().body("field", Matchers.contains("name"))
