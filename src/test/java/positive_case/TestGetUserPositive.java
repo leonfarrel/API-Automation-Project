a940bf5078e82ff305ba9fe547f694fa96c848d5
Leon Farrel <leonfarrel09@gmail.com>
2025-09-01 22:17:20 +0800

    Update Report

--- src/test/java/positive_case/TestGetUserPositive.java
+++ src/test/java/positive_case/TestGetUserPositive.java
@@ -20,7 +20,9 @@ public class TestGetUserPositive {
     public void testGetSpecificUserPositive(){
 
         RestAssured
-                .given().when().get("https://gorest.co.in/public/v2/users/8100528").then().log().all()
+                .given()
+                .header("Authorization", "Bearer 3df5a19b9214fbbfa6746286a8f7ea655592fa87c5b55d1b09b2ff7c7732bd7a")
+                .when().get("https://gorest.co.in/public/v2/users/8097968").then().log().all()
                 .assertThat().statusCode(200);
     }
 }
--- src/test/java/positive_case/TestPostUserPositive.java
+++ src/test/java/positive_case/TestPostUserPositive.java
@@ -11,7 +11,7 @@ public class TestPostUserPositive {
     public void testPostUserPositive(){
 
         String valueNama = "Leon Farrel";
-        String valueEmail = "leon1003@gmail.com";
+        String valueEmail = "leon1008@gmail.com";
         String valueGender ="male";
         String valueStatus ="active";
 
